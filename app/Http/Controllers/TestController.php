672196a1af3635cb427cd8bd879999b2317677ce
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-05-31 09:01:25 +0800

    The introduction of query builder

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Test;
 use App\Http\Requests\StoreTestRequest;
 use App\Http\Requests\UpdateTestRequest;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
@@ -13,7 +14,14 @@ class TestController extends Controller
      */
     public function index()
     {
-        //
+        $tests = DB::table('tests')
+                    ->select('name')
+                    ->get();
+        
+
+        return view('test',[
+            'tests' => $tests,
+        ]);
     }
 
     /**
---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TeSt</title>
+</head>
+<body>
+    @foreach ($tests as $test )
+        <h1>{{ $test->name }}</h1>
+    @endforeach
+
+    <h1>This is the added part</h1>
+
+
+</body>
+</html>
